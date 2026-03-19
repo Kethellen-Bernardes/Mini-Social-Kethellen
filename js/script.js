5682532ab7f6b845769021e6c66eb12e83eb8ef3
Kethellen-Bernardes <kethellen.9532532@aluno.mg.gov.br>
2026-03-19 14:19:24 +0000

    update js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
 let likeCount = 0;
+let curtido = false;//flag booleanna
 
 function curtir() {
-  likeCount++;
-  document.getElementById("likeCount").innerText = likeCount;
+  if(curtido == False){
+    likeCount++;
+    curtido = true;
+document.getElementById("likecount").innerText = likeCount
+}else{
+
+likeCount--;
+curtido = false;
+document.getElementById("likecount").innerText = likeCount
+
 }
+
+}
+
 
 document.getElementById("likeBtn").addEventListener("click", curtir);
